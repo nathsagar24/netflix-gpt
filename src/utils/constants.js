5301a8a9078283ee434041382a87ba2e7f51500d
nathsagar24 <nathsagar24@gmail.com>
2025-05-31 10:02:38 +0530

    Created Secondary Container

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,8 @@ export const LOGO="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_product
 
 export const USER_AVATAR="https://tse3.mm.bing.net/th?id=OIP.kYYbdJhBIh1SEi8MKTPYpgHaHa&pid=Api&P=0&h=220";
 
+export const IMG_CDN_URL="https://image.tmdb.org/t/p/w500";
+
 export const API_OPTIONS = {
     method: 'GET',
     headers: {
