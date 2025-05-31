33f4604b7bc8bcb72b004ae5b3b8dc71fd7b1284
nathsagar24 <nathsagar24@gmail.com>
2025-05-31 20:01:00 +0530

    Fixed issues

--- src/components/GPTSearch.js
+++ src/components/GPTSearch.js
@@ -7,8 +7,8 @@ const GPTSearch = () => {
   return (
     <>
         <div>
-            <div className='fixed -z-10'>
-        <img className='h-screen object-cover' src={BG_URL}
+            <div className='fixed md:absolute -z-10'>
+        <img className='h-screen w-screen object-cover' src={BG_URL}
     alt='logo'/>
     </div>
     <div className=''>
--- src/components/GPTSearchBar.js
+++ src/components/GPTSearchBar.js
@@ -30,7 +30,7 @@ const GPTSearchBar = () => {
   }
 
   return (
-    <div className='pt-[35%] flex justify-center'>
+    <div className='pt-[35%] md:pt-[10%] flex justify-center'>
         <form className='w-full md:w-1/2 bg-black grid grid-cols-12' onSubmit={(e)=>e.preventDefault()}>
             <input ref={searchText} type='text' className='p-4 m-4 col-span-9' placeholder={lang[langKey].gptSearchPlaceholder} />
             <button className='m-4 py-2 px-4 bg-red-700 text-white rounded-lg col-span-3' onClick={handleGPTSearchClick}>{lang[langKey].search}</button>
